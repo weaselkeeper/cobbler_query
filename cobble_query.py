597d9c202ebaa5554ac3fa70632374d73a1a1020
Jim Richardson <weaselkeeper@gmail.com>
2011-11-01 16:22:10 -0700

    add license info

--- cobble_query.py
+++ cobble_query.py
@@ -9,6 +9,23 @@
 
 """
 
+LICENSE="""
+Copyright (C) 2011 Jim Richardson <weaselkeeper@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License V2
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 import optparse
 import traceback
 import xmlrpclib
